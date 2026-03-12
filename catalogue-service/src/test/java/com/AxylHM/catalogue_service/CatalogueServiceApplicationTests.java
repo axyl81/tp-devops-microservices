7fae3f6f66a22d7c9fcb3808552a3cffeeb8b55f
Axyl HM <axylzizu@gmail.com>
2026-03-12 16:27:52 +0100

    Test: H2 setup and profiles for all services

--- catalogue-service/src/test/java/com/AxylHM/catalogue_service/CatalogueServiceApplicationTests.java
+++ catalogue-service/src/test/java/com/AxylHM/catalogue_service/CatalogueServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.AxylHM.catalogue_service;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // C'est ici que ça doit être !
 class CatalogueServiceApplicationTests {
 
 	@Test
--- paiement-service/src/test/java/com/AxylHM/paiement_service/PaiementServiceApplicationTests.java
+++ paiement-service/src/test/java/com/AxylHM/paiement_service/PaiementServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package com.AxylHM.paiement_service;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PaiementServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test si le contexte Spring de Paiement démarre bien avec H2
 	}
 
 }
